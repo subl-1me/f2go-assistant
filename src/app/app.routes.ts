6b52236473da472a42121631356114d4eba72e60
subl-1me <julio.ugalde404@gmail.com>
2025-04-27 10:00:38 -0600

    Missing change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { InvoicesComponent } from './components/invoices/invoices.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'reservations', component: ReservationsComponent },
   { path: 'invoices', component: InvoicesComponent },
+  { path: 'reports', component: ReportsComponent },
 ];
