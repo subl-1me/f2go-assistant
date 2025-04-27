b1ee8e8b1224c7684e543f48d755afd3d1ad2a5e
subl-1me <julio.ugalde404@gmail.com>
2025-04-27 05:12:17 -0600

    ReservationsView ~ Added collapse functionality to each component card

--- src/app/components/reservations/reservations.component.ts
+++ src/app/components/reservations/reservations.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-reservations',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './reservations.component.html',
-  styleUrl: './reservations.component.css'
+  styleUrl: './reservations.component.css',
 })
 export class ReservationsComponent {
+  isCurrentCardCollapsed = true;
+  isCheckCardCollapsed = true;
+  isRoutingCardCollapsed = true;
+  isAutoAssignCardCollapsed = true;
 
+  public toggleCurrentCardCollapse(): void {
+    this.isCurrentCardCollapsed = !this.isCurrentCardCollapsed;
+  }
+
+  public toggleCheckCardCollapse(): void {
+    this.isCheckCardCollapsed = !this.isCheckCardCollapsed;
+  }
+
+  public toggleRoutingCardCollapse(): void {
+    this.isRoutingCardCollapsed = !this.isRoutingCardCollapsed;
+  }
+
+  public toggleAutoAssignCardCollapse(): void {
+    this.isAutoAssignCardCollapsed = !this.isAutoAssignCardCollapsed;
+  }
 }
