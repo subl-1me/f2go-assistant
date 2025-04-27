41d22a72f6ddd83fa12beb9efb63876c879e524e
subl-1me <julio.ugalde404@gmail.com>
2025-04-27 10:11:23 -0600

    Login ~ Template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { InvoicesComponent } from './components/invoices/invoices.component';
 import { ReportsComponent } from './components/reports/reports.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: LoginComponent },
   { path: 'reservations', component: ReservationsComponent },
   { path: 'invoices', component: InvoicesComponent },
   { path: 'reports', component: ReportsComponent },
+  { path: 'login', component: LoginComponent },
 ];
