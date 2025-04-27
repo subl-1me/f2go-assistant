d9e6dcc4026fb76d31f5883097efadf454dc17c9
subl-1me <julio.ugalde404@gmail.com>
2025-04-27 07:14:39 -0600

    Check reservations view

---
+++ src/app/components/reservations/check-reservations/check-reservations.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CheckReservationsComponent } from './check-reservations.component';
+
+describe('CheckReservationsComponent', () => {
+  let component: CheckReservationsComponent;
+  let fixture: ComponentFixture<CheckReservationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CheckReservationsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CheckReservationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/reservations/check-reservations/check-reservations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-reservations',
+  imports: [],
+  templateUrl: './check-reservations.component.html',
+  styleUrl: './check-reservations.component.css'
+})
+export class CheckReservationsComponent {
+
+}
